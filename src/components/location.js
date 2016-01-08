51acdf4934f83dcffc6eac1c818e6424d29b5b7e
ajith <ajith@tsepak.com>
2016-01-08 19:34:24 +0530

    integrated react geosuggest, but the data is not filtered according to the type of information

--- src/components/location.js
+++ src/components/location.js
@@ -1,19 +1,74 @@
 import React from 'react';
 import TextField from 'material-ui/lib/text-field';
-import { Link } from 'react-router'
+import { Link } from 'react-router';
+import Geosuggest from 'react-geosuggest';
+import RaisedButton from 'material-ui/lib/raised-button';
 class Location extends React.Component {
 	constructor(props){
 		super(props);	
+        this.state = {
+            gmap: {},
+        };
 	}	
+    onCitySuggestSelect(location){
+        console.log('onCitySuggestSelect',location);
+    }
+    onLocalitySuggestSelect(location){
+        console.log('onLocalitySuggestSelect',location);
+    }
+    onAddressSuggestSelect(location){
+        console.log('onAddressSuggestSelect',location);
+    }
+    onPickLocation(){
+        if (navigator.geolocation) {
+            let _this = this;
+            console.log(_this);
+            let position = navigator.geolocation.getCurrentPosition(function(position){
+                console.log(position);
+                let map = new google.maps.Map(document.getElementById('map'), {
+                    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                    zoom: 6
+                }); 
+                _this.setState({
+                    gmap:map
+                });
+            });
+          } else {
+            // Browser doesn't support Geolocation\
+            console.error('Browser doesnt support Geolocation');
+          }
+        
+    }
 	render(){
 		return (<div style={this.props.styles.slide}>
-                    <TextField fullWidth={true}
-                        floatingLabelText="City" />
-                    <TextField fullWidth={true}
-                        floatingLabelText="Locality" />
-                    <TextField fullWidth={true}
-                        floatingLabelText="Address" />
-                    <Link  to="/timings" >Timings</Link>
+                    <Geosuggest 
+                        placeholder={"City"}
+                        className={"GeoSuggestList"}
+                        inputClassName={"GeoSuggestinput"}
+                        country={"in"}
+                        searchText={"hello"}
+                        onSuggestSelect={this.onCitySuggestSelect.bind(this)} />
+                    <Geosuggest 
+                        placeholder={"Locality"}
+                        className={"GeoSuggestList"}
+                        inputClassName={"GeoSuggestinput"}
+                        country={"in"}
+                        onSuggestSelect={this.onLocalitySuggestSelect.bind(this)} />
+                    <Geosuggest 
+                        placeholder={"Address"}
+                        className={"GeoSuggestList"}
+                        inputClassName={"GeoSuggestinput"}
+                        country={"in"}
+                        onSuggestSelect={this.onAddressSuggestSelect.bind(this)} />
+                    <div className={"pickLocation"}>
+                        <RaisedButton 
+                            primary={true} 
+                            label="PICK LOCATION"
+                            onTouchTap={this.onPickLocation.bind(this)} />
+                    </div>
+                    <div className={"Timings"}>
+                        <Link  to="/timings" >Timings</Link>
+                    </div>
                   </div>
                 );
 	}
