d93366eadc98c682c1b33412be143e154281f482
ajith <ajith@tsepak.com>
2016-01-11 13:47:58 +0530

    filtered Location>City && Location>Locality from suggestion, the Locality is based on the City boundary.

--- src/components/location.js
+++ src/components/location.js
@@ -8,17 +8,28 @@ class Location extends React.Component {
 		super(props);	
         this.state = {
             gmap: {},
+            selCity: {},
+            citySuggest:[],
+            selLocality:{},
+            selAddress:''
         };
 	}	
     onCitySuggestSelect(location){
-        console.log('onCitySuggestSelect',location);
+        console.log('onCitySuggestSelect',location); 
+        this.setState({
+            selCity:location
+        });      
     }
     onLocalitySuggestSelect(location){
         console.log('onLocalitySuggestSelect',location);
+        this.setState({
+            selLocality:location
+        });
+        if(!this.state.selCity.label){
+            console.log('city not set');
+        }
     }
-    onAddressSuggestSelect(location){
-        console.log('onAddressSuggestSelect',location);
-    }
+
     onPickLocation(){
         if (navigator.geolocation) {
             let _this = this;
@@ -37,29 +48,96 @@ class Location extends React.Component {
             // Browser doesn't support Geolocation\
             console.error('Browser doesnt support Geolocation');
           }
-        
     }
+
+
+    //City selection
+    getCitySuggestLabel(suggest){
+        let city = '',termsLength = 0;
+        termsLength = suggest.terms.length;
+        if( termsLength > 2){
+            city = suggest.terms[termsLength-3].value;
+        }     
+        return city;  
+    }
+    cityOnChange(keyWord){
+        console.log('city onChange');
+        this.setState({
+            citySuggest: [],
+        });
+    }
+    cityOnBlur(){
+        console.log('cityOnBlur');
+    }
+    skipCitySuggest(suggest){
+        let city = '',termsLength = 0;
+        termsLength = suggest.terms.length;
+        city = this.getCitySuggestLabel(suggest);  
+        let citySuggest = this.state.citySuggest;
+        if(citySuggest.indexOf(city)>=0){
+            return true;
+        }else{
+            citySuggest.push(city);
+            this.setState({
+                citySuggest: citySuggest
+            });
+            return false;
+        } 
+    }
+
+    //Locality Selection
+    getLocatlitySuggestLabel(suggest){
+        console.log('locality Label:',suggest);
+        let locality = [],termsLength = 0;
+        termsLength = suggest.terms.length;
+        suggest.terms.forEach(term => {
+            locality.push(term.value);
+        });
+        locality.splice(termsLength-3,termsLength);
+        let localityLabel = locality.join();
+        return localityLabel;
+    }
+    skipLocalitySuggest(suggest){
+        console.log('skipLocalitySuggest',suggest);
+        let city = '',termsLength = 0;
+        termsLength = suggest.terms.length;
+        city = this.getCitySuggestLabel(suggest);  
+        if(!this.state.selCity.label){
+            return false; //if city not selected yet
+        }
+        if(this.state.selCity && 
+           this.state.selCity.label &&
+           this.state.selCity.label == city){
+            return false; //city is selected, thus filtering localities only in the city
+        }else{
+            return true; //blocking localities outside the city boundary
+        }
+    }
+    
 	render(){
 		return (<div style={this.props.styles.slide}>
                     <Geosuggest 
                         placeholder={"City"}
                         className={"GeoSuggestList"}
                         inputClassName={"GeoSuggestinput"}
+                        autoActivateFirstSuggest={true}
                         country={"in"}
-                        searchText={"hello"}
+                        getSuggestLabel={this.getCitySuggestLabel.bind(this)}
+                        skipSuggest={this.skipCitySuggest.bind(this)}
+                        onChange={this.cityOnChange.bind(this)}
+                        onBlur={this.cityOnBlur.bind(this)}
                         onSuggestSelect={this.onCitySuggestSelect.bind(this)} />
                     <Geosuggest 
                         placeholder={"Locality"}
                         className={"GeoSuggestList"}
                         inputClassName={"GeoSuggestinput"}
+                        autoActivateFirstSuggest={true}
                         country={"in"}
+                        getSuggestLabel={this.getLocatlitySuggestLabel.bind(this)}
+                        skipSuggest={this.skipLocalitySuggest.bind(this)}
                         onSuggestSelect={this.onLocalitySuggestSelect.bind(this)} />
-                    <Geosuggest 
-                        placeholder={"Address"}
-                        className={"GeoSuggestList"}
-                        inputClassName={"GeoSuggestinput"}
-                        country={"in"}
-                        onSuggestSelect={this.onAddressSuggestSelect.bind(this)} />
+                    <TextField fullWidth={true}
+                        floatingLabelText="Address" />
                     <div className={"pickLocation"}>
                         <RaisedButton 
                             primary={true} 
