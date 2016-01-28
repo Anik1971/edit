4c100eea41cb9542c1e485c3fff369bd9956d5c1
Subramanyam S <shastri9999@gmail.com>
2016-01-28 04:54:56 +0000

    Adding gallery to profile page

--- src/components/profile/gallery.js
+++ src/components/profile/gallery.js
@@ -4,64 +4,97 @@ import GridTile from 'material-ui/lib/grid-list/grid-tile';
 import StarBorder from 'material-ui/lib/svg-icons/toggle/star-border';
 import IconButton from 'material-ui/lib/icon-button';
 import RaisedButton from 'material-ui/lib/raised-button';
+import SwipeableViews from 'react-swipeable-views';
 
-const gradientBg = 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)';
-const gridListStyle = {width: 500, height: 400, overflowY: 'auto', marginBottom: 24};
-const gallery = [
-  {
-    img: 'http://lorempixel.com/600/337/nature/',
-    title: 'Breakfast',
-    author: 'jill111',
-    featured: true,
+const tilesData = [{
+  img: 'http://lorempixel.com/600/337/nature/',
+  title: 'Breakfast',
+  author: 'jill111',
+  featured: true,
+}, {
+  img: 'http://lorempixel.com/500/337/nature/',
+  title: 'Tasty burger',
+  author: 'pashminu',
+}, {
+  img: 'http://lorempixel.com/400/300/nature/',
+  title: 'Camera',
+  author: 'Danson67',
+}, {
+  img: 'http://lorempixel.com/600/437/nature/',
+  title: 'Morning',
+  author: 'fancycrave1',
+  featured: true,
+}, {
+  img: 'http://lorempixel.com/600/300/nature/',
+  title: 'Hats',
+  author: 'Hans',
+}, {
+  img: 'http://lorempixel.com/300/300/nature/',
+  title: 'Hats',
+  author: 'Hans',
+}];
+
+const styles = {
+  slide: {
+    minHeight: 400,
+    backgroundSize: '85%',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'rgba(0,0,0,0.95)'
   },
-  {
-    img: 'http://lorempixel.com/500/337/nature/',
-    title: 'Tasty burger',
-    author: 'pashminu',
+  slideIndicator: {
+    borderRadius: '100%',
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    height: 10,
+    width: 10,
+    display: 'inline-block',
+    marginRight: 5
   },
-  {
-    img: 'http://lorempixel.com/400/300/nature/',
-    title: 'Camera',
-    author: 'Danson67',
-  },
-  {
-    img: 'http://lorempixel.com/600/437/nature/',
-    title: 'Morning',
-    author: 'fancycrave1',
-    featured: true,
-  },
-  {
-    img: 'http://lorempixel.com/600/300/nature/',
-    title: 'Hats',
-    author: 'Hans',
-  },
-  {
-    img: 'http://lorempixel.com/300/300/nature/',
-    title: 'Hats',
-    author: 'Hans',
+  indicatorContainer: {
+    backgroundColor: 'rgba(0,0,0,0.95)',
+    padding: 10,
+    textAlign: 'center',
   }
-];
+};
+const photos = tilesData.map((tile, index) => <div key={index} style={Object.assign({},styles.slide,{backgroundImage:"url(" + tile.img  +")"})}></div>);
 class Gallery extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      slideIndex: 0,
+    };
+
   }
+  getIndicatorStyle(index) {
+    var indicatorStyle = {
+      borderRadius: '100%',
+      backgroundColor: 'rgba(255,255,255,0.75)',
+      height: 10,
+      width: 10,
+      display: 'inline-block',
+      marginRight: 5
+    };
+    if (index == this.state.slideIndex)
+      indicatorStyle.backgroundColor = 'rgba(255,255,255,0.9)';
+    return indicatorStyle;
+  }
+  handleChange(value) {
+    this.setState({
+      slideIndex: value,
+    });
+  }
+
   render() {
-    return (<div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'}}>
-      {/*Grid list with all possible overrides*/}
-      <GridList
-        cols={2}
-        cellHeight={200}
-        padding={1}
-        style={gridListStyle}
-        >
-        {
-          gallery.map((tile,index) => <GridTile
-          cols={tile.featured ? 2 : 1}
-          rows={tile.featured ? 2 : 1}
-          key={index}><img src={tile.img} /></GridTile>)
-        }
-      </GridList>
-    </div>);
+    return (<div id="gallery">
+              <SwipeableViews
+                index={this.state.slideIndex}
+                onChangeIndex={this.handleChange.bind(this)}>
+                {photos}
+              </SwipeableViews>
+              <div style={styles.indicatorContainer}>
+                {tilesData.map((tile,index)=> <div key={index} style={this.getIndicatorStyle(index)}></div>)}
+              </div>
+            </div>);
   }
 }
 export default Gallery;
