76e1f95ac7e0bd7f8b6a9bc70b9a48fcf0def322
mudits <mudits1990@gmail.com>
2016-01-30 00:24:34 +0530

    Meridiem and time formating added.

--- src/components/edit/storetimings.js
+++ src/components/edit/storetimings.js
@@ -3,6 +3,19 @@ class StoreTimings extends React.Component {
 	constructor(props){
 		super(props);	
 	}	
+
+	formatTimings(time){
+		time = Math.floor(time/100) + ':' + time%100
+		return time 
+	}
+
+	getMeridiem(time){
+		var meridiem = 'AM'
+		if (time/1200 > 0){
+			meridiem = 'PM'
+		}
+		return meridiem
+	}
 	render(){
 		const days = {"Monday": [{"close": 2259, "open": 830}], "Tuesday": [{"close": 1259, "open": 830}, {"close": 2259, "open": 1400}], "Friday": [{"close": 2259, "open": 830}], "Wednesday": [{"close": 2259, "open": 830}], "Thursday": [{"close": 2259, "open": 830}], "Sunday": [{"close": 2259, "open": 830}], "Saturday": [{"close": 2259, "open": 830}]}	
 		const styles = {
@@ -21,7 +34,7 @@ class StoreTimings extends React.Component {
 		};
 
 		return (<div id="store-timings"> 
-                    {Object.keys(days).map((day,index)=><div style={styles.outerDivStyle} key={index}><div style={styles.circleStyle}>{day.substring(0,2)}</div> {days[day].map(timing=><div>{timing.open} <span>to</span> {timing.close}</div>)}</div>)}     
+                    {Object.keys(days).map((day,index)=><div style={styles.outerDivStyle} key={index}><div style={styles.circleStyle}>{day.substring(0,2)}</div> {days[day].map(timing=><div>{this.formatTimings(timing.open)} <span>{this.getMeridiem(timing.open)}</span><span>to</span> {this.formatTimings(timing.close)} <span>{this.getMeridiem(timing.close)}</span></div>)}</div>)}     
                 </div>);
 	}
 }
