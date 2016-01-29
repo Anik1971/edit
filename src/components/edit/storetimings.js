442683ddc1c9547d54803cdda867afbe0e1a7ce2
mudits <mudits1990@gmail.com>
2016-01-30 01:54:38 +0530

    Select and text box to change timing added.

--- src/components/edit/storetimings.js
+++ src/components/edit/storetimings.js
@@ -2,10 +2,13 @@ import React from 'react';
 class StoreTimings extends React.Component {
 	constructor(props){
 		super(props);	
+		this.state = {
+			days: 		 {"Monday": [{"close": 2259, "open": 830}], "Tuesday": [{"close": 1259, "open": 830}, {"close": 2259, "open": 1400}], "Friday": [{"close": 2259, "open": 830}], "Wednesday": [{"close": 2259, "open": 830}], "Thursday": [{"close": 2259, "open": 830}], "Sunday": [{"close": 2259, "open": 830}], "Saturday": [{"close": 2259, "open": 830}]}	
+		};
 	}	
 
 	formatTimings(time){
-		time = Math.floor(time/100) + ':' + time%100
+		time = Math.floor(time/100) + ':' + (time%100 === 0 ? '00' : time%100)
 		return time 
 	}
 	// getDays(day, index){
@@ -19,8 +22,15 @@ class StoreTimings extends React.Component {
 		}
 		return meridiem
 	}
+	changeStoreTime()
+	{
+		debugger;
+		this.setState({
+			"days" : {"Monday": [{"close": 2259, "open": 930}], "Tuesday": [{"close": 1259, "open": 830}, {"close": 2259, "open": 1400}], "Friday": [{"close": 2259, "open": 830}], "Wednesday": [{"close": 2259, "open": 830}], "Thursday": [{"close": 2259, "open": 830}], "Sunday": [{"close": 2259, "open": 830}], "Saturday": [{"close": 2259, "open": 830}]}	
+		});
+	}
 	render(){
-		const days = {"Monday": [{"close": 2259, "open": 830}], "Tuesday": [{"close": 1259, "open": 830}, {"close": 2259, "open": 1400}], "Friday": [{"close": 2259, "open": 830}], "Wednesday": [{"close": 2259, "open": 830}], "Thursday": [{"close": 2259, "open": 830}], "Sunday": [{"close": 2259, "open": 830}], "Saturday": [{"close": 2259, "open": 830}]}	
+		const selectOptions = ['All days', 'Weekdays', 'Weekends']
 		const styles = {
 			addTimingsDivStyle:{
 				position:'fixed',
@@ -46,10 +56,17 @@ class StoreTimings extends React.Component {
 				textAlign: 'center'
 			}
 		};
-
+		const days = this.state.days;
 		return (<div id="store-timings">
-					<div style={styles.addTimingsDivStyle} key={1}></div>
-					<div key={2} style={styles.scrollableDivStyle}> 
+					<div style={styles.addTimingsDivStyle} key={1}><div key={3}>
+						<select>{selectOptions.map((option, index)=><option key={index}>{option}</option>)}</select>
+						<div> 
+							<input type="number"></input> to <input type="number"></input>
+							<button onclick={this.changeStoreTime.bind(this)}>Change</button>
+						</div>
+					</div>
+					</div>
+						<div key={2} style={styles.scrollableDivStyle}> 
                 		{Object.keys(days).map((day,index)=><div style={styles.outerDivStyle} key={index}><div style={styles.circleStyle}>{day.substring(0,2)}</div> {days[day].map((timing,index)=><div key={index}>{this.formatTimings(timing.open)} <span>{this.getMeridiem(timing.open)}</span><span> to</span> {this.formatTimings(timing.close)} <span>{this.getMeridiem(timing.close)}</span></div>)}</div>)}     
             		</div>
             	</div>);
