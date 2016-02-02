00833d657714ce1bdf93805346c1dfe9448b190b
ajith <ajith@tsepak.com>
2016-02-02 17:25:09 +0530

    location warning

--- src/components/edit/location.js
+++ src/components/edit/location.js
@@ -154,10 +154,8 @@ class Location extends React.Component {
                 zoom: 18,
                 scaleControl: false,
                 scrollwheel: false,
-                scrollwheel: false,
                 navigationControl: false,
                 mapTypeControl: false,
-                scaleControl: false,
                 draggable: false,
                 disableDoubleClickZoom: false
             }); 
@@ -435,6 +433,10 @@ class Location extends React.Component {
                           <div style={{padding:2}}> 
                             <span className="small">Update store location</span>                           
                             <div id="storeMap" className={"storeMap"}></div>
+                            <div className="mapWarning">
+                                <span className="mapWarningText small"><b className="black">{"Warning: "}</b>{"The store location will be updated to the location shown in the map."}</span>
+                            </div>
+
                             <RaisedButton 
                                 fullWidth = { true}
                                 secondary={true} 
