fe596eb181da8d33c00b30d640c927d0f3ec7889
ajith <ajith@tsepak.com>
2016-02-05 18:16:20 +0530

    delivery enabled changed basic html checkbox

--- src/components/edit/delivery.js
+++ src/components/edit/delivery.js
@@ -250,7 +250,7 @@ class Delivery extends React.Component {
       });
     }
   }
-  onDeliveryStatusToggle(e, deliveryStatus){
+  onDeliveryStatusToggle(deliveryStatus){
       if(deliveryStatus){
         this.setState({
           homeDelivery: '',
@@ -491,9 +491,12 @@ class Delivery extends React.Component {
   }
 	render(){
     let geoInitialVal = this.state.geoInitialVal;
-		return (
-            <div style={this.props.styles.slide}>
-                <Checkbox
+    let homeDeliveryChecked = '';
+    let homeDeliveryCheckBox = <div className="row"><label>{"Home Delivery "}<input onChange={this.onDeliveryStatusToggle.bind(this,true)} style={{float:'right'}} type="checkbox" value={false} /></label></div>;
+    if(this.state.homeDeliveryEnabled){
+      homeDeliveryCheckBox = <div className="row"><label>{"Home Delivery "}<input onChange={this.onDeliveryStatusToggle.bind(this,false)} style={{float:'right'}} type="checkbox" value={true} checked="checked" /></label></div>;
+    }
+    /*<Checkbox
                   name="deliveryStatus"
                   value="delivery"
                   label="Home Delivery"
@@ -501,7 +504,10 @@ class Delivery extends React.Component {
                   onCheck={this.onDeliveryStatusToggle.bind(this)}
                   disabled={this.state.toggleDisabled}
                   labelPosition="left"
-                  className="row"/>  
+                  className="row"/>*/
+		return (
+            <div style={this.props.styles.slide}>                
+                {homeDeliveryCheckBox}
                 <div className={this.state.homeDelivery}>              
                   <SelectField value={this.state.deliveryPricing}
                         floatingLabelText="Delivery Pricing"
